0032abff0c9aa51f7c556720552e0836c4bf12ba
Giovanne Silveira <123279931+GiovanneGSH@users.noreply.github.com>
2025-05-19 11:13:13 -0300

    criando crud tabelas

--- dbfrancisco.sql
+++ dbfrancisco.sql
@@ -10,7 +10,17 @@ nome varchar(50) not null,
 senha varchar(12) not null,
 primary key(codUsu));
  
-insert into tbUsuarios(nome,senha)
-	values('senac','senac');
+insert into tbUsuarios(nome,senha)values('senac','senac');
  
 select * from tbUsuarios;
+
+update tbUsuarios set nome='senac',senha='111111111111' where codUsu = 1;
+
+
+-- pesquisa filtrada por código
+
+select * from tbUsuarios where codUsu = 1;
+
+-- pesquisa filtrada por nome
+
+select * from tbUsuarios where nome like '%" + usuarios + "%';
